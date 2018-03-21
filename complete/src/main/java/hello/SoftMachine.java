ee3d73891afa03e521b317dc0f48abe1dc1926e8
syntel <huntmac10@gmail.com>
2018-03-21 11:54:21 -0400

    Request restock method and timeout

--- complete/src/main/java/hello/SoftMachine.java
+++ complete/src/main/java/hello/SoftMachine.java
@@ -1,7 +1,15 @@
 package hello;
 
+
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.ArrayList;
+
 public class SoftMachine {
     private String companyName;
+    private String machineId;
 
     public String getCompanyName() {
         return this.companyName;
@@ -9,5 +17,23 @@ public class SoftMachine {
 
     public void setCompanyName(BadCompany company) {
         this.companyName = company.getCompanyName();
+    }
+
+    public void requestRestock() {
+        try {
+            makeCall();
+        } catch (Exception e) {
+            System.out.println("Request for restock failed.");
+        }
+    }
+
+    private void makeCall() throws ResourceAccessException {
+            String transactionUrl = "http://192.168.88.123:8080/requestRestock";
+            UriComponentsBuilder builder = UriComponentsBuilder
+                    .fromUriString(transactionUrl).queryParam("id", machineId);
+
+            RestTemplate restTemplate = new RestTemplate();
+            String response = restTemplate.getForObject(builder.toUriString(), String.class);
+            System.out.println(response);
     }
 }
